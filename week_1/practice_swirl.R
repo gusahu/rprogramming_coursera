6231f26678d78fed89c374a5704016928a9f7acf
gusahu <gusahu18@gmail.com>
2020-12-11 01:35:46 -0300

    Commit

---
+++ week_1/practice_swirl.R
@@ -0,0 +1,13 @@
+##########################################
+# Week 1: Practical eR exercise in swrirl 
+##########################################
+
+# 1. Install swirl
+# install.packages("swirl")
+# packageVersion("swirl")
+# 2. Load swirl
+library(swirl)
+# 3. Install the R Progroamming course
+# install_from_swirl("R Programming")
+
+swirl()
